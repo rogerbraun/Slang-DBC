876c3697fa64c438d4cc7fcb88298ef14f324743
thmuelle <thmuelle>
2009-09-07 06:45:20 +0000

    *** empty log message ***

--- src/de/uni_tuebingen/wsi/ct/slang2/dbc/server/DBC_Server.java
+++ src/de/uni_tuebingen/wsi/ct/slang2/dbc/server/DBC_Server.java
@@ -4234,8 +4234,8 @@ public class DBC_Server implements Runnable, DBC_KeyAcceptor {
 		try {
 			PreparedStatement stmt = connection.prepareStatement(
 					"SELECT content FROM words WHERE words.id IN" +
-					"(SELECT word_id from word_list_elements where word_list_elements.assignation_id IN " +
-					"(SELECT id FROM assignations WHERE tr_tempus & wa1));",
+					"(SELECT word_id FROM word_list_elements WHERE word_list_elements.assignation_id IN " +
+					"(SELECT id FROM assignations WHERE tr_wortart1 & wa1));",
 					ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
 			//stmt.setBytes(1, wa1);
 			stmt.setInt(1, wortArt1AsInt);

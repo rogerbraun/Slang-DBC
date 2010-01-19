a5902249bff909a6162a1c21408e7c2dc7ede1b6
jurczyk <jurczyk>
2010-01-19 09:01:25 +0000

    *** empty log message ***

--- src/de/uni_tuebingen/wsi/ct/slang2/dbc/server/DBC_Server.java
+++ src/de/uni_tuebingen/wsi/ct/slang2/dbc/server/DBC_Server.java
@@ -882,6 +882,8 @@ public class DBC_Server implements Runnable, DBC_KeyAcceptor {
 			}
 		}
 		return chapter;
+		
+		//testkommentar
 	}
 
     public synchronized void deleteChapter(Integer chapterID)
